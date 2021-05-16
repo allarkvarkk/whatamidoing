79a1483f0f9aab3b161edb8b4daad1d95ce2165e
jacob hauptman <allarkvarkk@gmail.com>
2021-05-16 18:07:38 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>Hello World</h1>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          IDk what's happening
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
